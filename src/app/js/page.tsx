68385496b6df0022919adfca13ef89a360dbf1cb
dillonzellis <dillonzellis@gmail.com>
2025-05-11 17:59:18 -0400

    style: add title to SidebarNav

--- src/app/js/page.tsx
+++ src/app/js/page.tsx
@@ -2,12 +2,28 @@ import Link from "next/link";
 import MaxWidthWrapper from "@/components/UI/MaxWidthWrapper";
 import TypographyH1 from "@/components/Typograpy/TypographyH1";
 
+type LinkComponentProps = {
+  href: string;
+  children: React.ReactNode;
+};
+
+function LinkComponent({ href, children }: LinkComponentProps) {
+  return (
+    <Link href={href} className="text-xl font-bold hover:underline">
+      {children}
+    </Link>
+  );
+}
+
 export default function JsPage() {
   return (
     <MaxWidthWrapper>
-      <TypographyH1 className="mb-4">JS</TypographyH1>
+      <TypographyH1 className="mb-4">Javascript</TypographyH1>
       <hr className="pb-4" />
-      <Link href="js/loops">Loops</Link>
+      <div className="flex flex-col gap-2">
+        <LinkComponent href="js/loops">Loops</LinkComponent>
+        <LinkComponent href="js/arrays">Arrays</LinkComponent>
+      </div>
     </MaxWidthWrapper>
   );
 }
